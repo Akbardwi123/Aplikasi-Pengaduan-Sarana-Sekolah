d64be4bb83ff73e64cb5fcdfa49a7f13c4c14555
Akbardwi123 <akbardwipebriansyah@gmail.com>
2026-02-16 17:43:44 +0700

    controller and blade

---
+++ resources/views/layouts/nav-item/siswa-guest.blade.php
@@ -0,0 +1,8 @@
+@guest('siswa')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">Login</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">Register</a>
+    </li>
+@endguest
